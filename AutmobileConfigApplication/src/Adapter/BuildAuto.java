7f805edc28405960be4fade796cb2b58de8e6f11
Thomas Lao <laothomas01@gmail.com>
2022-10-17 00:50:24 -0700

    making sure CRUD functions are working as they should

--- AutmobileConfigApplication/src/Adapter/BuildAuto.java
+++ AutmobileConfigApplication/src/Adapter/BuildAuto.java
@@ -5,20 +5,17 @@ import Exception.Fix1to100;
 
 import java.io.IOException;
 
-//give BuildAuto class access to Automobile class instance encapsulated in ProxyAutomobile
+
 public class BuildAuto extends ProxyAutomobile implements CreateAuto, UpdateAuto, FixAuto {
 
-	//used to keep an on-going loop until all recorded exceptions are recorded
-	boolean problemFixed = false;
+    //used to keep an on-going loop until all recorded exceptions are fixed
+    boolean problemFixed = false;
 
-	@Override
-	public void buildAuto(String fileName) throws IOException {
-		FileIO io = new FileIO(fileName);
-		//attempt to populate the automobile instance with data read from the text file
-		a1 = io.loadAutomotive();
-		System.out.println(a1);
-
-
+    @Override
+    public void buildAuto(String fileName) throws IOException {
+        FileIO io = new FileIO(fileName);
+        //attempt to populate the automobile instance with data read from the text file
+        a1 = io.loadAutomotive();
 //		int[] errNums = io.readArrayOfErrors("listOfErrors.txt");
 //
 //		//@TODO: convert a while-loop
@@ -30,52 +27,60 @@ public class BuildAuto extends ProxyAutomobile implements CreateAuto, UpdateAuto
 //			problemFixed = fix(-1);
 //		}
 //		System.out.println(a1);
-	}
+        System.out.println(a1.getOptionClassInstance(0, 0));
+        System.out.println("BEFORE DELETING OPTION SET:" + a1);
+        a1.deleteOptionSetInstance(0);
+        System.out.println("AFTER DELETING OPTION SET BY INDEX:" + a1);
+
+        a1.deleteOptionSetInstance("Brakes");
+        System.out.println("AFTER DELETING OPTION SET BY NAME:" + a1);
+
+    }
 
 
-	//search for a valid automobile name and if valid, print the name
-	@Override
-	public void printAuto(String modelName) {
-		if (a1.getName().equals(modelName)) {
-			System.out.println(a1.toString());
-		}
-		System.out.println("Automobile cannot be found!");
-	}
+    //search for a valid automobile name and if valid, print the name
+    @Override
+    public void printAuto(String modelName) {
+        if (a1.getName().equals(modelName)) {
+            System.out.println(a1.toString());
+        }
+        System.out.println("Automobile cannot be found!");
+    }
 
-	@Override
-	public void updateOptionSetName(String modelName, String OptionSetName, String newName) {
-		if (a1.getName().equals(modelName)) {
-			a1.updateOptionSetName(OptionSetName, newName);
-		}
-	}
+    @Override
+    public void updateOptionSetName(String modelName, String OptionSetName, String newName) {
+        if (a1.getName().equals(modelName)) {
+            a1.updateOptionSetName(OptionSetName, newName);
+        }
+    }
 
-	@Override
-	public void updateOptionPrice(String modelName, String OptionSetName, String OptionName, float newPrice) {
-		if (a1.getName().equals(modelName)) {
-			a1.updateOptionPrice(OptionSetName, OptionName, newPrice);
-		}
-	}
+    @Override
+    public void updateOptionPrice(String modelName, String OptionSetName, String OptionName, float newPrice) {
+        if (a1.getName().equals(modelName)) {
+            a1.updateOptionPrice(OptionSetName, OptionName, newPrice);
+        }
+    }
 
 
-	/**
-	 * Plan of attack:
-	 * - externally: loop through a fixed number of lines from the list of errors and parse the first element
-	 * - funnel that error into this function and we will determine the fix to the program based on the base made in the switch statement
-	 * - we will right now solve 1 error because i need to get this lab turned in, and work on SQL assignment. then backtrack to this to write cleaner code!
-	 */
+    /**
+     * Plan of attack:
+     * - externally: loop through a fixed number of lines from the list of errors and parse the first element
+     * - funnel that error into this function and we will determine the fix to the program based on the base made in the switch statement
+     * - we will right now solve 1 error because i need to get this lab turned in, and work on SQL assignment. then backtrack to this to write cleaner code!
+     */
 
-	//each error number is read from a .txt file and directed to a specific fix
-	@Override
-	public boolean fix(int errNo) throws IOException {
-		Fix1to100 f1 = new Fix1to100();
-		switch (errNo) {
-			case 0:
-				f1.fixMissingPriceFromTextFile();
-				break;
-			default:
-				break;
-		}
-		return true;
+    //each error number is read from a .txt file and directed to a specific fix
+    @Override
+    public boolean fix(int errNo) throws IOException {
+        Fix1to100 f1 = new Fix1to100();
+        switch (errNo) {
+            case 0:
+                f1.fixMissingPriceFromTextFile();
+                break;
+            default:
+                break;
+        }
+        return true;
 
-	}
+    }
 }
--- AutmobileConfigApplication/src/Model/OptionSet.java
+++ AutmobileConfigApplication/src/Model/OptionSet.java
@@ -2,198 +2,212 @@ package Model;
 
 import java.io.Serializable;
 
+/**
+ * [O] = works
+ * [X] = broken
+ */
 class OptionSet implements Serializable {
 
-	private Option options[];
-	private String name;
+    private Option options[];
+    private String name;
 
-	protected OptionSet(String name, int size) {
-		setName(name);
-		options = new Option[size];
-		//initialize array of options with new instances of empty options
-		for (int i = 0; i < size; i++) {
-			options[i] = new Option("BLANK OPTION", 0);
-		}
-	}
+    //[O]
+    protected OptionSet(String name, int size) {
+        setName(name);
+        options = new Option[size];
+        //initialize array of options with new instances of empty options
+        for (int i = 0; i < size; i++) {
+            options[i] = new Option("BLANK OPTION", 0);
+        }
+    }
 
-	protected OptionSet(String name) {
-		this(name, 0);
-	}
+    //[O]
+    protected OptionSet(String name) {
+        this(name, 0);
+    }
 
-	protected OptionSet(int size) {
-		this("BLANK OPTION", size);
-	}
+    //[O]
+    protected OptionSet(int size) {
+        this("BLANK OPTION", size);
+    }
 
-	protected OptionSet() {
-		this("BLANK OPTION", 0);
-	}
+    //[O]
+    protected OptionSet() {
+        this("BLANK OPTION", 0);
+    }
 
-	//---------------------- OPTION SET INSTANCE CRUD OPERATIONS ----------------
-	protected String getName() {
-		return name;
-	}
-	//retrieve Option array
-	protected Option[] getOptions() {
-		return options;
-	}
-	//retrieve Option array length
-	protected int getOptionsSize() {
-		if (getOptions().length == 0) {
-			return 0;
-		}
-		return getOptions().length;
-	}
+    //---------------------- OPTION SET INSTANCE CRUD OPERATIONS ----------------
+    //[O]
+    protected String getName() {
+        return name;
+    }
 
-	protected void setName(String name) {
-		this.name = name;
-	}
-	//replace current array of Option instances with new array
-	protected void updateOptions(Option[] opts) {
-		this.options = opts;
-	}
-	//we print out our option set data in the same format to CarConfigs.txt
-	protected Option createOption(String n, float p) {
-		return new Option(n, p);
-	}
+    //retrieve Option array
+    //[O]
+    protected Option[] getOptions() {
+        return options;
+    }
 
-	protected Option createOption(String n) {
-		return new Option(n);
-	}
+    //retrieve Option array length
+    //[O]
+    protected int getOptionsSize() {
+        if (getOptions().length == 0) {
+            return 0;
+        }
+        return getOptions().length;
+    }
 
-	protected Option createOption() {
-		return new Option();
-	}
+    //[O]
+    protected void setName(String name) {
+        this.name = name;
+    }
 
-	//retrieve Option class instance by index
-	protected Option getOption(int i) {
+    //we print out our option set data in the same format to CarConfigs.txt
+    protected Option createOption(String n, float p) {
+        return new Option(n, p);
+    }
 
-//		return getOptions()[i];
-		if (getOptions()[i] == null || i < 0 || i >= getOptionsSize()) {
-			return new Option();
-		}
-		return getOptions()[i];
-	}
+    protected Option createOption(String n) {
+        return new Option(n);
+    }
 
-	//basic retrieve Option instance by name
-	protected Option getOption(String n) {
-		for (int i = 0; i < getOptionsSize(); i++) {
-			if (getOption(i).getName().equals(n)) {
-				return getOption(i);
-			}
-		}
-		return new Option();
-	}
+    protected Option createOption() {
+        return new Option();
+    }
 
-	//search by index and replace current Option instance with new Option instance
-	protected void setOption(int i, Option o) {
-		if (i < 0 || i >= getOptionsSize()) {
-			System.out.println("OPTION DOES NOT EXIST!");
-		}
-		getOptions()[i] = o;
-	}
+    //retrieve Option class instance by index
 
-	//search by name and replace current Option instance with new Option instance
-	protected void setOption(String n, Option o) {
-		for (int i = 0; i < getOptionsSize(); i++) {
-			if (getOption(i).getName().equals(n)) {
-				setOption(i, o);
-			}
-		}
-		System.out.println("OPTION DOES NOT EXIST!");
-	}
+    protected Option getOption(int i) {
 
-	//search by index, input replacing name and price, and use those parameters to input into a empty Option class instancee
-	protected void setOption(int searchIndex, String replaceName, float replaceOptionPrice) {
-		setOption(searchIndex, new Option(replaceName, replaceOptionPrice));
-	}
+        if (getOptions()[i] == null || i < 0 || i >= getOptionsSize()) {
+            return new Option();
+        }
+        return getOptions()[i];
+    }
 
-	//search by name, input replacing name and price, and use those parameters to input to empty Option class instance
-	protected void setOption(String searchOptionName, String replaceName, float replaceOptionPrice) {
-		setOption(searchOptionName, new Option(replaceName, replaceOptionPrice));
-	}
+    //basic retrieve Option instance by name
+    protected Option getOption(String n) {
+        for (int i = 0; i < getOptionsSize(); i++) {
+            if (getOption(i).getName().equals(n)) {
+                return getOption(i);
+            }
+        }
+        return new Option();
+    }
 
-	//search by index and update option instance to empty Option class instance
-	protected void deleteOption(int i) {
-		//change specified option at index i = to empty Option class instance
-		setOption(i, new Option());
-	}
+    //search by index and replace current Option instance with new Option instance
+    protected void setOption(int searchIndex, Option o) {
+        if (searchIndex < 0 || searchIndex >= getOptionsSize()) {
+            System.out.println("OPTION DOES NOT EXIST!");
+        }
+        getOptions()[searchIndex] = o;
+    }
 
-	//search by name and update option instance to to empty Option class instance
-	protected void deleteOption(String name) {
-		setOption(name, new Option());
-	}
+    //search by name and replace current Option instance with new Option instance
+    protected void setOption(String searchName, Option o) {
+        for (int i = 0; i < getOptionsSize(); i++) {
+            if (getOption(i).getName().equals(searchName)) {
+                setOption(i, o);
+            }
+        }
+        System.out.println("OPTION DOES NOT EXIST!");
+    }
 
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
+    //search by index, input replacing name and price, and use those parameters to input into a empty Option class instancee
+    protected void setOption(int searchIndex, String replaceName, float replaceOptionPrice) {
+        setOption(searchIndex, new Option(replaceName, replaceOptionPrice));
+    }
+
+    //search by name, input replacing name and price, and use those parameters to input to empty Option class instance
+    protected void setOption(String searchOptionName, String replaceName, float replaceOptionPrice) {
+        setOption(searchOptionName, new Option(replaceName, replaceOptionPrice));
+    }
+
+    //search by index and update option instance to empty Option class instance
+    protected void deleteOption(int i) {
+        //change specified option at index i = to empty Option class instance
+        setOption(i, new Option());
+    }
+
+    //search by name and replace option instance with empty Option class instance
+
+    protected void deleteOption(String name) {
+        setOption(name, new Option());
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < getOptionsSize(); i++) {
+            sb.append(getName() + "|" + getOption(i) + "\n");
+        }
+
 //		sb.append(getName());
 //		sb.append(getOption(0).getName() + " " + getOption(0).getPrice());
 //		sb.append(getName() + "|");
-		for (int i = 0; i < getOptionsSize(); i++) {
-			sb.append(getOption(i).toString());
-		}
 //
-		for (int i = 0; i < getOptionsSize(); i++) {
-			if (i == getOptionsSize() - 1) {
-				sb.append(getOption(i).getName() + "");
-			} else {
-				sb.append(getOption(i).getName() + " ");
-			}
-		}
-		sb.append("|");
-		for (int i = 0; i < getOptionsSize(); i++) {
-			if (i == getOptionsSize() - 1) {
-				sb.append(getOption(i).getPrice() + "");
-			} else {
-				sb.append(getOption(i).getPrice() + " ");
-			}
-		}
-		return sb.toString();
-	}
+//        for (int i = 0; i < getOptionsSize(); i++) {
+//            if (i == getOptionsSize() - 1) {
+//                sb.append(getOption(i).getName() + "");
+//            } else {
+//                sb.append(getOption(i).getName() + " ");
+//            }
+//        }
+//        sb.append("|");
+//        for (int i = 0; i < getOptionsSize(); i++) {
+//            if (i == getOptionsSize() - 1) {
+//                sb.append(getOption(i).getPrice() + "");
+//            } else {
+//                sb.append(getOption(i).getPrice() + " ");
+//            }
+//        }
+        return sb.toString();
+    }
 
-	//inner class
-	protected class Option implements Serializable {
+    //inner class
+    protected class Option implements Serializable {
 
-		private String name;
-		private float price;
+        private String name;
+        private float price;
 
-		protected Option(String n, float p) {
-			name = n;
-			price = p;
-		}
+        protected Option(String n, float p) {
+            name = n;
+            price = p;
+        }
 
-		protected Option(String n) {
-			this(n, 0);
-		}
+        protected Option(String n) {
+            this(n, 0);
+        }
 
-		protected Option(float p) {
-			this("BLANK OPTION", p);
-		}
+        protected Option(float p) {
+            this("BLANK OPTION", p);
+        }
 
-		protected Option() {
-			this("BLANK OPTION", 0f);
-		}
+        protected Option() {
+            this("BLANK OPTION", 0f);
+        }
 
-		protected String getName() {
-			return name;
-		}
+        protected String getName() {
+            return name;
+        }
 
-		protected void setName(String name) {
-			this.name = name;
-		}
+        protected void setName(String name) {
+            this.name = name;
+        }
 
-		protected float getPrice() {
-			return price;
-		}
+        protected float getPrice() {
+            return price;
+        }
 
-		protected void setPrice(float price) {
-			this.price = price;
-		}
+        protected void setPrice(float price) {
+            this.price = price;
+        }
 
-		public String toString() {
-			StringBuffer sb = new StringBuffer(getName() + " " + getPrice() + "|");
-			return sb.toString();
-		}
+        public String toString() {
+            StringBuffer sb = new StringBuffer();
+            sb.append(getName() + " " + getPrice());
+            return sb.toString();
+        }
 
-	}
+    }
 
 }

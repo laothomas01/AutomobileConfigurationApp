5e39b0ecc4e773ed4b64696dee8f78495782946c
Thomas Lao <laothomas01@gmail.com>
2022-10-20 04:18:10 -0700

    handling storage of multiple vehicles via random test data and defined option sets in text file

--- AutmobileConfigApplication/src/Adapter/BuildAuto.java
+++ AutmobileConfigApplication/src/Adapter/BuildAuto.java
@@ -6,62 +6,82 @@ import Exception.Fix1to100;
 
 import java.io.IOException;
 import java.util.LinkedHashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 //what is the purpose of this class???
-public class BuildAuto extends ProxyAutomobile implements CacheAuto,CreateAuto, UpdateAuto, FixAuto {
+public class BuildAuto extends ProxyAutomobile implements CacheAuto, CreateAuto, UpdateAuto, FixAuto {
 
-	//used to keep an on-going loop until all recorded exceptions are fixed
-	boolean problemFixed = false;
-	boolean finishedBuilding = false;
+    //used to keep an on-going loop until all recorded exceptions are fixed
+    boolean problemFixed = false;
+    boolean finishedBuilding = false;
+    Random rand = new Random();
+    //used to cache any built automobile by the user
+    LinkedHashMap<String, Automobile> cacheAutos;
 
+    //for building 1 auto
 
-	private LinkedHashMap<String, Automobile> cacheAutos = new LinkedHashMap<>();
+    // OR we can leveraged this method to loop through a list of fileNames and build an automobile for each
+    @Override
+    public void buildAuto(String fileName) throws IOException {
+        cacheAutos = new LinkedHashMap<>();
+        FileIO io = new FileIO(fileName);
+        //populate a1 with all its car configurations(we do not have more configurations for more cars. lets just hardcode this)
+//        a1 = io.loadAutomotive();
 
+        String[] testCarNames = {"Abarth", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Bugatti", "Cadillac", "Chevrolet", "Chrysler", "Citroën", "Dacia", "Daewoo", "Daihatsu", "Dodge", "Donkervoort", "DS", "Ferrari", "Fiat", "Fisker", "Ford", "Honda", "Hummer", "Hyundai", "Infiniti", "Iveco", "Jaguar", "Jeep", "Kia", "KTM", "Lada", "Lamborghini", "Lancia", "Land Rover", "Landwind", "Lexus", "Lotus", "Maserati", "Maybach", "Mazda", "McLaren", "Mercedes-Benz", "MG", "Mini", "Mitsubishi", "Morgan", "Nissan", "Opel", "Peugeot", "Porsche", "Renault", "Rolls-Royce", "Rover", "Saab", "Seat", "Skoda", "Smart", "SsangYong", "Subaru", "Suzuki", "Tesla", "Toyota", "Volkswagen", "Volvo"};
 
-	//for building 1 auto
-
-	// OR we can leveraged this method to loop through a list of fileNames and build an automobile for each
-	@Override
-	public void buildAuto(String fileName) throws IOException {
-
-		FileIO io = new FileIO(fileName);
-		//attempt to populate the automobile instance with data read from the text file
-		a1 = io.loadAutomotive();
-
-
-		/**
-		 * BUILDING CAR INSTANCES WORKFLOW
-		 *    - user enters the menu
-		 *    - loop menu until user exits
-		 *    - have text file with an array of names, prices, a number of option sets, and each option set line containing
-		 *    an array of option data(this does not account for specific cars with specific parts. its pure customizable)
-		 *    - when user wants to build auto, load car config data into the automobile(we only have configs for 1 right now).
-		 - user will have access to that automobile instance's data
-		 - when customizing, automobile instance will have a list of user's choice( we will do something with this later)
-		 - when user finishes cache the automobile into a linked hashmap
+        System.out.println("LENGTH:" + testCarNames.length);
+        a1 = io.loadAutomotive();
 
 
-		 here we will make dummy instances for now
-		 */
-		String choice = null;
-		Scanner sc = new Scanner(System.in);
-		do {
-			System.out.println("Press [Y/N]");
-			choice = sc.nextLine();
-			if (choice.equalsIgnoreCase("y")) {
-				System.out.println("BUILDING");
+        /**
+         * BUILDING CAR INSTANCES WORKFLOW
+         *    - user enters the menu
+         *    - loop menu until user exits
+         *    - have text file with an array of names, prices, a number of option sets, and each option set line containing
+         *    an array of option data(this does not account for specific cars with specific parts. its pure customizable)
+         *    - when user wants to build auto, load car config data into the automobile(we only have configs for 1 right now).
+         - user will have access to that automobile instance's data
+         - when customizing, automobile instance will have a list of user's choice( we will do something with this later)
+         - when user finishes cache the automobile into a linked hashmap
 
-			} else if (choice.equalsIgnoreCase("N")) {
-				System.out.println("BYE");
-				finishedBuilding = true;
-			} else {
-				System.out.println("INVALID INPUT!");
-			}
-		}
-		while (finishedBuilding == false);
-////		a2 = new Automobile("Batmobile", 100000, 5);
-//
+
+         here we will make dummy instances for now
+         */
+
+        String choice = null;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("BUILDING AUTOMOBILE:");
+            System.out.println("Press [Y/N]");
+            choice = sc.nextLine();
+            if (choice.equalsIgnoreCase("y")) {
+                int randInt = rand.nextInt(testCarNames.length - 0) + 0;
+                //handle collisons
+                if (cacheAutos.containsKey(testCarNames[randInt])) {
+                    cacheAutos.put(testCarNames[randInt], cacheAutos.get(testCarNames[randInt]));
+                } else {
+                    //put in a new car name and automobile instance
+                    Automobile a = io.loadAutomobileOptionSets();
+                    a.setName(testCarNames[randInt]);
+                    a.setBasePrice(rand.nextFloat() * (100000f - 50000f) + 50000f);
+                    cacheAutos.put(a.getName(), a);
+                }
+            } else if (choice.equalsIgnoreCase("n")) {
+                System.out.println("SAVED AUTOMOBILES");
+                System.out.println("BYE");
+                finishedBuilding = true;
+            } else {
+                System.out.println("INVALID INPUT!");
+            }
+        } while (finishedBuilding == false);
+
+        for (String s : testCarNames) {
+            if (cacheAutos.containsKey(s)) {
+                System.out.println(cacheAutos.get(s).toString());
+            }
+        }
 //		int[] errNums = io.readArrayOfErrors("listOfErrors.txt");
 //		//@TODO: convert a while-loop
 //		if (!problemFixed) {
@@ -74,17 +94,24 @@ public class BuildAuto extends ProxyAutomobile implements CacheAuto,CreateAuto, 
 //		addAutomobile(a1);
 
 
-	}
-	//for building multiple autos
+    }
+    //for building multiple autos
 
 
-	@Override
-	public void printAuto(String modelName) {
+    @Override
+    public void printAuto(String modelName) {
+//        System.out.println(cacheAutos.get(modelName));
+    }
 
-	}
+    @Override
+    public void printAutos(LinkedHashMap<String, Automobile> autos) {
+        for (String key : autos.keySet()) {
+            printAuto(key);
+        }
+    }
 
 
-	// CRUD for automobile instances
+    // CRUD for automobile instances
 //	public LinkedHashMap<String, Automobile> getAutomobiles() {
 //		return automobiles;
 //	}
@@ -112,50 +139,49 @@ public class BuildAuto extends ProxyAutomobile implements CacheAuto,CreateAuto, 
 //		}
 //	}
 
-	@Override
-	public void updateOptionSetName(String modelName, String OptionSetName, String newName) {
-		if (a1.getName().equals(modelName)) {
-			a1.updateOptionSetName(OptionSetName, newName);
-		}
-	}
+    @Override
+    public void updateOptionSetName(String modelName, String OptionSetName, String newName) {
+        if (a1.getName().equals(modelName)) {
+            a1.updateOptionSetName(OptionSetName, newName);
+        }
+    }
 
-	@Override
-	public void updateOptionPrice(String modelName, String OptionSetName, String OptionName, float newPrice) {
-		if (a1.getName().equals(modelName)) {
-			a1.updateOptionPrice(OptionSetName, OptionName, newPrice);
-		}
-	}
+    @Override
+    public void updateOptionPrice(String modelName, String OptionSetName, String OptionName, float newPrice) {
+        if (a1.getName().equals(modelName)) {
+            a1.updateOptionPrice(OptionSetName, OptionName, newPrice);
+        }
+    }
 
 
-	/**
-	 * Plan of attack:
-	 * - externally: loop through a fixed number of lines from the list of errors and parse the first element
-	 * - funnel that error into this function and we will determine the fix to the program based on the base made in the switch statement
-	 * - we will right now solve 1 error because i need to get this lab turned in, and work on SQL assignment. then backtrack to this to write cleaner code!
-	 */
+    /**
+     * Plan of attack:
+     * - externally: loop through a fixed number of lines from the list of errors and parse the first element
+     * - funnel that error into this function and we will determine the fix to the program based on the base made in the switch statement
+     * - we will right now solve 1 error because i need to get this lab turned in, and work on SQL assignment. then backtrack to this to write cleaner code!
+     */
 
-	//each error number is read from a .txt file and directed to a specific fix
-	@Override
-	public boolean fix(int errNo) throws IOException {
-		Fix1to100 f1 = new Fix1to100();
-		switch (errNo) {
-			case 0:
-				f1.fixMissingPriceFromTextFile();
-				break;
-			default:
-				break;
-		}
-		return true;
+    //each error number is read from a .txt file and directed to a specific fix
+    @Override
+    public boolean fix(int errNo) throws IOException {
+        Fix1to100 f1 = new Fix1to100();
+        switch (errNo) {
+            case 0:
+                f1.fixMissingPriceFromTextFile();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 
-	}
+    @Override
+    public void storeAuto(Automobile a) {
+//        cacheAutos.put(a.getName(), a);
+    }
 
-	@Override
-	public void storeAuto(Automobile a) {
+    @Override
+    public void storeAuto(String name) {
 
-	}
-
-	@Override
-	public void storeAuto(String name) {
-
-	}
+    }
 }
--- AutmobileConfigApplication/src/Adapter/CreateAuto.java
+++ AutmobileConfigApplication/src/Adapter/CreateAuto.java
@@ -1,17 +1,22 @@
 package Adapter;
 
+import Model.Automobile;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
 /**
  * Creates and Reads automotive methods.
- *
+ * <p>
  * How would I describe what this interface is? what is its functionality and purpose????
- *
+ * <p>
  * Why this???
  */
 public interface CreateAuto {
-	void buildAuto(String fileName) throws IOException;
+    void buildAuto(String fileName) throws IOException;
 
-	void printAuto(String modelName);
+    void printAuto(String modelName);
+
+    void printAutos(LinkedHashMap<String, Automobile> autos);
 }
